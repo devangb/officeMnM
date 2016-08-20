70474bb5c4edf051da5a2ce13fd052d384da18c0
Devang Bacharwar <devanggtgr8@gmail.com>
2016-08-20 10:26:32 +0530

    User email login

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User implements UserInterface {
+	
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
+	
+	/**
+	 * @ORM\Column(type="string", unique=true)
+	 */
+	private $email;
+	
+	
+	private $username;
+	
+	public function getUsername() {
+		return $this->username;
+	}
+	
+	public function getRoles() {
+		return ['ROLE_USER'];
+	}
+	
+	public function getPassword() {
+		
+	}
+	
+	public function getSalt() {
+		
+	}
+	
+	public function eraseCredentials() {
+		
+	}
+	
+	public function setUsername($username) {
+		$this->username = $username;
+	}
+}
