7abebbac75e915b7ab72e008e5d6709456c95758
Devang Bacharwar <devanggtgr8@gmail.com>
2016-08-26 12:48:59 +0530

    Ignores

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,14 +18,13 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
         	new Sentry\SentryBundle\SentryBundle(),
             new AppBundle\AppBundle(),
-        	new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            //$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
